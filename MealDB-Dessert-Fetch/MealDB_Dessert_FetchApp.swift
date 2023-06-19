1f7c78b9ce7788d71c7faad2cbfad5ac61fae837
Kevin <kevin_jeon@icloud.com>
2023-06-18 21:24:07 -0400

    Initial Commit

---
+++ MealDB-Dessert-Fetch/MealDB_Dessert_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDB_Dessert_FetchApp.swift
+//  MealDB-Dessert-Fetch
+//
+//  Created by Kevin Mattocks on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MealDB_Dessert_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
