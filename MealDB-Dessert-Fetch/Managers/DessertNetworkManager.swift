9ac878a42bd351d8c438e13c829dc2b1fb591674
Kevin <kevin_jeon@icloud.com>
2023-11-03 16:14:20 -0400

    Add comments on the static keyword utilized for the network manager

--- MealDB-Dessert-Fetch/Managers/DessertNetworkManager.swift
+++ MealDB-Dessert-Fetch/Managers/DessertNetworkManager.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class DessertNetworkManager {
     
-    
-    
+    //Static. Can call this function/method without making an instance
+    //Static keyword is creating a type constant
     static func getDesserts() async throws -> DessertResponse? {
         
         guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else { throw MealError.invalidURL }
